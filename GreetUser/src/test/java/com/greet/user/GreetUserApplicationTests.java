115eae961ee2a67ad18e3c2524c6a30e221c812e
Syed <Syed@host.docker.internal>
2020-09-20 19:55:39 +0530

    Commited Project

---
+++ GreetUser/src/test/java/com/greet/user/GreetUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greet.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
